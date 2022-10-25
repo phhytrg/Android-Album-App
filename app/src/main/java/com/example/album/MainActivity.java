39b918db715939e8867ba65284984e4e0f687b1d
Thanh <ntnthanh.109@gmail.com>
2022-10-25 23:23:50 +0700

    tao layout_detail_image

--- app/src/main/java/com/example/album/MainActivity.java
+++ app/src/main/java/com/example/album/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.album;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav=(BottomNavigationView)findViewById(R.id.bottom_nav);
+        bottomNav.setItemIconTintList(null);
+
+
+
+
     }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onPopUpMenuClick(menuItem);
+            }
+        });
+        popup.inflate(R.menu.ic_more_submenu);
+        popup.show();
+    }
+    public boolean onPopUpMenuClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.details:
+                Toast.makeText(this, "details", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.add:
+                Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.set:
+                Toast.makeText(this, "set", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rename:
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
 }
