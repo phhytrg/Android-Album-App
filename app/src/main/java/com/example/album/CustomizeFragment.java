90629c6375a8f7ed1c366399cedad310a9d09682
cobemuadong <truong.pht.7080@gmail.com>
2022-10-29 21:04:56 +0700

    merging error, not yet fixed

--- app/src/main/java/com/example/album/CustomizeFragment.java
+++ app/src/main/java/com/example/album/CustomizeFragment.java
@@ -3,10 +3,25 @@ package com.example.album;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
-public class CustomizeFragment extends PreferenceFragment {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.viewmodel.CreationExtras;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class CustomizeFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.custom_preferences);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+    @NonNull
+    @Override
+    public CreationExtras getDefaultViewModelCreationExtras() {
+        return super.getDefaultViewModelCreationExtras();
     }
 }
