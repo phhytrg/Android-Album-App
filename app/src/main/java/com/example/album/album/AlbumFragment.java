48350a1a8f0fa0740af153cb715c2676fcd876c5
cobemuadong <truong.pht.7080@gmail.com>
2022-11-02 20:23:44 +0700

    adding some material designs

--- app/src/main/java/com/example/album/album/AlbumFragment.java
+++ app/src/main/java/com/example/album/album/AlbumFragment.java
@@ -16,11 +16,18 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.MenuHost;
 import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.album.MainActivity;
 import com.example.album.R;
 import com.example.album.item_decoration.GridSpacingItemDecoration;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AlbumFragment extends Fragment{
 
@@ -29,9 +36,17 @@ public class AlbumFragment extends Fragment{
 
     String[] albumName = {"Camera","Videos", "Favorites","Screens","Locations","Download", "Collages",
             "Picnic", "Friends", "Selfie", "Memes"};
-    int[] albumImages = {R.drawable.photo1, R.drawable.photo2, R.drawable.photo10,
-            R.drawable.photo4, R.drawable.cat1, R.drawable.photo6, R.drawable.photo3,
-            R.drawable.photo5, R.drawable.photo8, R.drawable.photo7, R.drawable.photo9};
+    int[] albumImages = {R.drawable.photo1,
+            R.drawable.photo2,
+            R.drawable.photo10,
+            R.drawable.photo4,
+            R.drawable.cat1,
+            R.drawable.photo6,
+            R.drawable.photo3,
+            R.drawable.photo5,
+            R.drawable.photo8,
+            R.drawable.photo7,
+            R.drawable.photo9};
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,13 +59,18 @@ public class AlbumFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.album_layout,container, false).getRootView();
 
-        setUpToolBar(view);
-
+        boolean flag = setUpToolBar(view);
         MenuHost menuHost = requireActivity();
         menuHost.addMenuProvider(new MenuProvider() {
             @Override
             public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
                 menuInflater.inflate(R.menu.album_option,menu);
+                if(flag)
+                {
+                    ((MainActivity)getActivity())
+                            .getSupportActionBar()
+                            .setTitle(R.string.albums);
+                }
             }
 
             @Override
@@ -67,6 +87,7 @@ public class AlbumFragment extends Fragment{
                 }
             }
 
+
         });
         return view;
     }
@@ -76,11 +97,80 @@ public class AlbumFragment extends Fragment{
         super.onViewCreated(view, savedInstanceState);
 //        GridAdapter gridAdapter = new GridAdapter(requireContext(), albumName, albumImages);
 
+        //Set Adapter for RecyclerView
         AlbumAdapter albumAdapter = new AlbumAdapter();
         recyclerView = view.findViewById(R.id.album_list);
         recyclerView.setAdapter(albumAdapter);
+        //Set Layout Manager
         recyclerView.setLayoutManager(new GridLayoutManager(requireContext(),3));
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(3,8,false));
+        //Set Item Decoration
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(3,24,false));
+
+        //Navigation ToolBar's implement
+        MaterialToolbar navigationBar = getActivity().findViewById(R.id.navigation_bar);
+        navigationBar.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.album_navigation_bar,menu);
+
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                NavController navController =
+                        Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+                if (id == R.id.galleryFragment){
+                    return NavigationUI.onNavDestinationSelected(menuItem,navController) ||
+                            AlbumFragment.super.onOptionsItemSelected(menuItem);
+                }
+//                else if(id == R.id.btn_albums){
+//
+//                }
+//                else if(id == R.id.btn_privacy){
+//
+//                }
+                return false;
+            }
+        });
+
+
+//        navigationBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                int id = item.getItemId();
+//                NavController navController =
+//                        Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+//                if (id == R.id.galleryFragment){
+//                    return NavigationUI.onNavDestinationSelected(item,navController) ||
+//                            AlbumFragment.super.onOptionsItemSelected(item);
+//                }
+//                else if(id == R.id.btn_albums){
+//
+//                }
+//                else if(id == R.id.btn_privacy){
+//
+//                }
+//                return false;
+//            }
+//        });
+
+        NavController navController = Navigation.findNavController(view);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph()).build();
+
+        Toolbar toolbar = view.findViewById(R.id.navigation_bar);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(
+                    @NonNull NavController navController
+                    , @NonNull NavDestination navDestination
+                    , @Nullable Bundle bundle) {
+                toolbar.setTitle("");
+            }
+        });
+
 //        gridView = (GridView) view.findViewById(R.id.album_list);
 //        gridView.setAdapter(gridAdapter);
 
@@ -93,12 +183,15 @@ public class AlbumFragment extends Fragment{
 //        });
     }
 
-    private void setUpToolBar(View view){
-        Toolbar toolbar = view.findViewById(R.id.album_app_bar);
+    private boolean setUpToolBar(View view){
+//        Toolbar toolbar = view.findViewById(R.id.album_app_bar);
         AppCompatActivity activity = (AppCompatActivity)getActivity();
+        Toolbar toolbar = getActivity().findViewById(R.id.app_bar);
         if(activity != null){
             activity.setSupportActionBar(toolbar);
+            return true;
         }
+        return false;
     }
 
     //    public void showAlbumOption(View v){
