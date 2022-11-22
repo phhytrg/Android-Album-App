353435e6c26c0463fc150519fe7d381fd9436e3a
cobemuadong <truong.pht.7080@gmail.com>
2022-11-22 19:10:55 +0700

    delete album now available

--- app/src/main/java/com/example/album/ImageUri.java
+++ app/src/main/java/com/example/album/ImageUri.java
@@ -1,9 +1,14 @@
 package com.example.album;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 
 import androidx.annotation.NonNull;
 
@@ -15,5 +20,49 @@ public class ImageUri {
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
         return Uri.parse(path);
+    }
+
+    @SuppressLint("NewApi")
+    public static Bitmap blurRenderScript(Context context,Bitmap smallBitmap, int radius) {
+        try {
+            smallBitmap = RGB565toARGB888(smallBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                smallBitmap.getWidth(), smallBitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        RenderScript renderScript = RenderScript.create(context);
+
+        Allocation blurInput = Allocation.createFromBitmap(renderScript, smallBitmap);
+        Allocation blurOutput = Allocation.createFromBitmap(renderScript, bitmap);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(renderScript,
+                Element.U8_4(renderScript));
+        blur.setInput(blurInput);
+        blur.setRadius(radius); // radius must be 0 < r <= 25
+        blur.forEach(blurOutput);
+
+        blurOutput.copyTo(bitmap);
+        renderScript.destroy();
+
+        return bitmap;
+    }
+
+    private static Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
     }
 }
--- app/src/main/java/com/example/album/album/AlbumAdapter.java
+++ app/src/main/java/com/example/album/album/AlbumAdapter.java
@@ -5,13 +5,19 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
@@ -19,15 +25,24 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.album.R;
+import com.example.album.SplitToolbar;
 import com.google.android.material.imageview.ShapeableImageView;
 
-public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>
-        implements View.OnClickListener, View.OnLongClickListener {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
 
     public final static int LINEAR_LAYOUT = 0;
     public final static int GRID_LAYOUT = 1;
     private int layoutType;
     private Context context;
+    public final static int CHANGED_MODE = 1;
+    public final static int UNCHANGED_MODE = 0;
+
+    private int currentMode = UNCHANGED_MODE;
     Fragment fragment;
 
     public void setLayoutType(int layoutType) {
@@ -43,15 +58,18 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         this.navController = navController;
         this.context = context;
         this.fragment = fragment;
+        selectedItems = new ArrayList<>();
     }
 
     NavController navController;
     //data field
-    String[] albumNames = {"Camera","Videos", "Favorites","Screens","Locations","Download", "Collages",
-            "Picnic", "Friends", "Selfie", "Memes"};
-    int[] albumImages = {R.drawable.photo1, R.drawable.photo2, R.drawable.photo10,
-            R.drawable.photo4, R.drawable.cat1, R.drawable.photo6, R.drawable.photo3,
-            R.drawable.photo5, R.drawable.photo8, R.drawable.photo7, R.drawable.photo9};
+    List<String> albumNames = new ArrayList<>(Arrays.asList("Camera", "Videos", "Favorites", "Screens", "Locations", "Download", "Collages",
+            "Picnic", "Friends", "Selfie", "Memes"));
+    List<Integer> albumImages = new ArrayList<>(Arrays.asList(R.drawable.photo1, R.drawable.photo2,
+            R.drawable.photo10, R.drawable.photo4, R.drawable.cat1, R.drawable.photo6,
+            R.drawable.photo3, R.drawable.photo5, R.drawable.photo8, R.drawable.photo7,
+            R.drawable.photo9));
+    List<Integer>selectedItems;
 
     @NonNull
     @Override
@@ -71,33 +89,52 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
 
     @Override
     public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
-        holder.shapeableImageView.setImageResource(albumImages[position]);
-        holder.albumName.setText(albumNames[position]);
+        holder.shapeableImageView.setImageResource(albumImages.get(position));
+        holder.albumName.setText(albumNames.get(position));
+        if(currentMode == CHANGED_MODE){
+            holder.checked.setVisibility(View.VISIBLE);
+        }
+        else{
+            holder.checked.setVisibility(View.GONE);
+        }
+        if(currentMode == CHANGED_MODE){
+            if(selectedItems.contains(position)){
+                holder.checked.setChecked(true);
+            }
+            else{
+                holder.checked.setChecked(false);
+            }
+        }
     }
 
     private void onItemClick(AlbumViewHolder holder){
         if(navController == null){
             return;
         }
-        String albumName = holder.albumName.getText().toString();
-        NavDirections action = AlbumFragmentDirections
-                .actionAlbumFragmentToDetailAlbumFragment(albumName);
-        Navigation.findNavController(holder.itemView).navigate(action);
+        if(currentMode == UNCHANGED_MODE) {
+            String albumName = holder.albumName.getText().toString();
+            NavDirections action = AlbumFragmentDirections
+                    .actionAlbumFragmentToDetailAlbumFragment(albumName);
+            Navigation.findNavController(holder.itemView).navigate(action);
+        }
+        else{
+            holder.checked.setChecked(!holder.checked.isChecked());
+            //Blur item here
+            if(holder.checked.isChecked()){
+                selectedItems.add(holder.getAdapterPosition());
+                //Blur item here
+            }
+            else{
+                selectedItems.remove(holder.getAdapterPosition());
+                holder.isCheckedFlag = true;
+                //Back
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-        return albumNames.length;
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        return false;
+        return albumNames.size();
     }
 
     class AlbumViewHolder extends RecyclerView.ViewHolder {
@@ -105,56 +142,127 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         TextView albumName;
         TextView numImages;
         ConstraintLayout item;
+        CheckBox checked;
+
+        boolean isCheckedFlag = false;
+
         public AlbumViewHolder(@NonNull View itemView) {
             super(itemView);
             shapeableImageView = itemView.findViewById(R.id.album_image);
             albumName = itemView.findViewById(R.id.album_name);
             item = itemView.findViewById(R.id.album_item);
-            if(layoutType == GRID_LAYOUT){
+            checked = itemView.findViewById(R.id.selected_item);
+
+            checked.setOnClickListener(v -> {
+                //Blur item here
+                if(checked.isChecked()){
+                    selectedItems.add(getAdapterPosition());
+                    //Blur item here
+                }
+                else{
+                    selectedItems.remove(getAdapterPosition());
+                    isCheckedFlag = true;
+                    //Back
+                }
+            });
+
+            if (layoutType == GRID_LAYOUT) {
             }
-            if(layoutType == LINEAR_LAYOUT){
+            if (layoutType == LINEAR_LAYOUT) {
                 numImages = itemView.findViewById(R.id.number_of_images);
             }
 
             itemView.setOnClickListener(v -> onItemClick(this));
-            if(layoutType == LINEAR_LAYOUT){
+            if (layoutType == LINEAR_LAYOUT) {
                 numImages.setText(Integer.toString(100));
             }
             itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                public final SplitToolbar navigationBar =
+                        fragment.getActivity().findViewById(R.id.navigation_bar);
+
                 @Override
                 public boolean onLongClick(View v) {
+                    if(currentMode == CHANGED_MODE)
+                        return false;
+                    currentMode = CHANGED_MODE;
+                    for(int i =0 ;i< navigationBar.getMenu().size(); ++i){
+                        navigationBar.getMenu().getItem(i).setEnabled(false);
+                    }
+                    for (int i = 0; i < getItemCount(); i++) {
+                        notifyItemChanged(i);
+                    }
                     showDialog();
-
                     return true;
+                }
+
+                private void showDialog() {
+                    final Dialog dialog = new Dialog(context, R.style.Theme_Album);
+                    dialog.setContentView(R.layout.selected_option);
+                    WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+                    params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                    params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                    params.format = PixelFormat.TRANSLUCENT;
+                    params.gravity = Gravity.TOP;
+                    params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+                    dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                    dialog.getWindow().setAttributes(params);
+
+                    final ImageButton backButton
+                            = dialog.findViewById(R.id.go_back_option);
+                    final ImageButton deleteButton
+                            = dialog.findViewById(R.id.delete_option);
+                    final CheckBox selectAll
+                            = dialog.findViewById(R.id.select_all_option);
+
+                    backButton.setOnClickListener(v -> {
+                        dialog.dismiss();
+                        currentMode = UNCHANGED_MODE;
+                        checked.setVisibility(View.GONE);
+                        for (int i = 0; i < getItemCount(); i++) {
+                            notifyItemChanged(i);
+                        }
+                        for(int i =0 ;i< navigationBar.getMenu().size(); ++i){
+                            navigationBar.getMenu().getItem(i).setEnabled(true);
+                        }
+                    });
+
+                    deleteButton.setOnClickListener(v -> {
+                        selectedItems.sort(Collections.reverseOrder());
+                        while (!selectedItems.isEmpty()) {
+                            int selectedIndex = selectedItems.get(0);
+                            selectedItems.remove(0);
+                            albumImages.remove(selectedIndex);
+                            albumNames.remove(selectedIndex);
+                            notifyItemRemoved(selectedIndex);
+                        }
+                    });
+
+                    selectAll.setOnClickListener(v -> {
+                        if (selectAll.isChecked()) {
+                            allSelected();
+                        } else {
+                            allUnselected();
+                        }
+                    });
+
+                    dialog.show();
+                }
+
+                private void allSelected() {
+                    selectedItems.clear();
+                    for (int i = getItemCount() - 1; i >= 0; --i) {
+                        selectedItems.add(i);
+                        notifyItemChanged(i);
+                    }
+                }
+
+                private void allUnselected() {
+                    selectedItems.clear();
+                    for (int i = getItemCount() - 1; i >= 0; --i) {
+                        notifyItemChanged(i);
+                    }
                 }
             });
         }
-    }
-
-    private void showDialog(){
-
-//        final BottomSheetDialog dialog = new BottomSheetDialog(context);
-//        dialog.setContentView(R.layout.multi_items_selected);
-//        dialog.set
-        final Dialog dialog = new Dialog(context, R.style.Theme_Album);
-        dialog.setContentView(R.layout.multi_items_selected);
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-
-        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        params.width = WindowManager.LayoutParams.MATCH_PARENT;
-        params.format = PixelFormat.TRANSLUCENT;
-
-        params.gravity = Gravity.TOP;
-        dialog.getWindow().setAttributes(params);
-
-//        Window window = dialog.getWindow();
-//        WindowManager.LayoutParams wlp
-//                = window.getAttributes();
-//        wlp.width =
-//                wlp.gravity = Gravity.TOP;
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-//        window.setAttributes(wlp);
-        dialog.show();
     }
 }
